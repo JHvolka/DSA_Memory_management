e4aceb9922de5c9fce7db3f411808179111ead3c
Jakub Hvolka <jhvolka@gmail.com>
2021-02-16 21:37:57 +0100

    some progress in implementation and documentation

--- BFMM.c
+++ BFMM.c
@@ -10,11 +10,11 @@ heapNode_t *find_best_fit(unsigned int size) {
         if (tmp->tree.left && tmp->tree.left->tree.size >= size)
             tmp = tmp->tree.left;
 
-        // Else use this node if sufficient
+            // Else use this node if sufficient
         else if (tmp->tree.size >= size)
             break;
 
-        // Else larger size node needed
+            // Else larger size node needed
         else if (tmp->tree.right)
             tmp = tmp->tree.right;
     }
@@ -28,31 +28,79 @@ heapNode_t *find_best_fit(unsigned int size) {
 }
 
 void memory_init(void *ptr, unsigned int size) {
-
     Heap = (unsigned char *) ptr;
     heap_root = (heapNode_t *) ptr;
 
-    // Initialize list
+    // Initialize list part
     heap_root->list.prev = NULL;
     heap_root->list.next = NULL;
     heap_root->list.used = 0;
 
-    // Initialize tree
+    // Initialize tree part
     heap_root->tree.parent = NULL;
     heap_root->tree.left = NULL;
     heap_root->tree.right = NULL;
     heap_root->tree.color = BLACK;
+    // Only list part is overhead
     heap_root->tree.size = size - sizeof(heapList_t);
 }
 
-void *memory_alloc(unsigned int size) {
+void*memory_alloc(unsigned int size) { // TODO check whole function
 
+    heapNode_t * remaining_block;
+
+    // Check if memory was initialized and if any memory is free for allocation
+    if (Heap == NULL || heap_root == NULL) {
+        return NULL;
+    }
+
+    // Find free memory space
+    heapNode_t *new_block = find_best_fit(size);
+
+    // Return NULL if no available block of given size is available
+    if (new_block == NULL)
+        return NULL;
+
+
+
+    // Tag new block as used
+    new_block->list.used = 0;
+
+    // Remove from tree of free memory blocks
+    delete_node(new_block);
+
+    // Split up found memory, if remainder will be of usable size. Enough space
+    // for new heapNode needs to be left after _size_ of memory is subtracted.
+    // Even if exactly `sizeof(heapNode_t)` space is left over, the part of node
+    // used for tree can still be allocated.
+    if (new_block->tree.size >= size + sizeof(heapNode_t)) {
+
+        // Create block with remaining space and insert it after new_block in list TODO better explanation
+        remaining_block = new_block + size + sizeof(heapNode_t); // todo check if sizeof is correct
+        remaining_block->list.used = 0;
+        remaining_block->list.next = new_block->list.next;
+        remaining_block->list.prev = new_block;
+
+        // Add remaining block into tree of free memory
+        insert_node(remaining_block);
+    } else{
+
+    }
+
+    // Return address where tree starts. Tree node part is no longer needed, and
+    // can be used by the caller
+    return (void*) &new_block->tree;
 }
 
 int memory_free(void *valid_ptr) {
+    // Assignment says we can assume this is never fails, but... ¯\_(ツ)_/¯
+    assert( memory_check(valid_ptr) );
+
+
     return 0;
 }
 
 int memory_check(void *ptr) {
+    // TODO implement
     return 0;
 }
--- BFMM.h
+++ BFMM.h
@@ -2,9 +2,10 @@
 
 #include <stdlib.h>
 #include <stdint.h>
+#include <assert.h>
 
 /** @file BFMM.h
- *
+ * TODO documentation
  */
 
 /**
@@ -16,29 +17,30 @@ enum color_t {
 };
 
 /**
- * A structure to represent doubly linked list for memory management.
+ * A structure to represent doubly linked list for memory management. ABC
  *
  * This part of heap node is not deleted when given memory segment is in use, as
  * it is required for merging freed blocks of memory.
  */
 typedef struct HEAP_LIST {
-    struct HEAP_NODE *prev;
-    struct HEAP_NODE *next;
-    uint8_t used;
+    struct HEAP_NODE *prev; /**< pointer to previous node */
+    struct HEAP_NODE *next; /**< pointer to next node */
+    uint8_t used;           /**< flag if block is allocated or free */
 } heapList_t;
 
 /**
  * A structure to represent red-black tree node for memory management.
  *
  * This part of heap node may be overwritten when given memory segment is in
- * use.
+ * use. It creates an overhead only if new tree node doesn't fit at the end of
+ * block that would have been split up, and thus the space is left unused.
  */
 typedef struct HEAP_TREE {
-    struct HEAP_NODE *parent;
-    struct HEAP_NODE *left;
-    struct HEAP_NODE *right;
-    enum color_t color;
-    unsigned int size;
+    struct HEAP_NODE *parent; /**< parent node pointer */
+    struct HEAP_NODE *left;   /**< smaller node pointer */
+    struct HEAP_NODE *right;  /**< bigger node pointer */
+    enum color_t color;       /**< node color (red or black) */
+    unsigned int size;        /**< size of available memory in block */
 } heapTree_t;
 
 /**
@@ -48,13 +50,16 @@ typedef struct HEAP_TREE {
  *      tree - red-black tree
  */
 typedef struct HEAP_NODE {
+    /** Node of doubly linked list. Used to keep relative position of blocks */
     heapList_t list;
-    heapTree_t tree;
+    heapTree_t tree; /**< A node of red-black tree for fast best fit search. */
 } heapNode_t;
 
+/// Global heap pointer
+static unsigned char *Heap = NULL;
 
-static unsigned char *Heap;
-static heapNode_t *heap_root;
+/// Global pointer to free heap blocks (BST)
+static heapNode_t *heap_root = NULL;
 
 
 /**
@@ -64,33 +69,41 @@ static heapNode_t *heap_root;
  * @param[in] size of data
  * @return pointer to data location or NULL if not found
  */
-heapNode_t *find_best_fit(unsigned int size);;
+heapNode_t *find_best_fit(unsigned int size);
+
+/**
+ * Inserts a node into red-black tree of free memory blocks. Node size is taken as key.
+ * @param[in] node_insert
+ * TODO finish documentation
+ */
+void insert_node(heapNode_t * node_insert);
+void delete_node(heapNode_t * node_delete);
 
 //==============================================================================
 
 /**
- * TODO
+ * TODO documentation
  * @param ptr
  * @param size
  */
 void memory_init(void *ptr, unsigned int size);
 
 /**
- * TODO
+ * TODO documentation
  * @param[in] size
  * @return
  */
 void *memory_alloc(unsigned int size);
 
 /**
- * TODO
+ * TODO documentation
  * @param valid_ptr
  * @return
  */
 int memory_free(void *valid_ptr);
 
 /**
- * TODO
+ * TODO documentation
  * @param ptr
  * @return
  */
